f04b21df93283a9bb2c81ecc9a6f6d3f7c49dd3b
Coderilz <116507723+Coderilz@users.noreply.github.com>
2023-06-09 19:28:43 +0100

    tracker kinda done

--- dash/app.py
+++ dash/app.py
@@ -5,7 +5,12 @@ app = Dash(__name__, use_pages=True)
 
 app.layout = html.Div([
     html.Div(
-        html.H1("Crypto Tracker")
+        html.H1("Crypto Tracker",
+                style={'margin-top': '50px',
+                       'text-transform': 'uppercase',
+                       'font-size': '40px',
+                       'color': 'black',
+                       'text-align': 'center'})
     ),
     html.Div(
         [
@@ -18,7 +23,7 @@ app.layout = html.Div([
         ]
     ),
     dash.page_container
-])
+], style={"width": "100%", "overflow": "hidden"})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- dash/pages/tracker.py
+++ dash/pages/tracker.py
@@ -8,19 +8,50 @@ dash.register_page(__name__, path="/tracker")
 
 layout = html.Div([
     html.Div([
-        html.H2("Options"),
-        html.P("Pick a crypto and date range to track"),
-        html.Label("Choose a crypto"),
+        html.H2("Options",
+                style={'margin-top': '50px',
+                       'text-transform': 'uppercase',
+                       'font-size': '20px',
+                       'color': 'black',
+                       'text-align': 'center'}),
+        html.P("Pick a crypto and date range to track",
+               style={'text-align': 'center',
+                      'color': 'black'
+                      }),
+        html.Img(src="assets/settings.png",
+                 style={
+                     'width': '320px'
+                 }),
+        html.Label("Choose a crypto",
+                   style={
+                       'margin-left': '20px',
+                       'color': 'black',
+                       'font-weight': 'bold'}),
         dcc.Dropdown(["ethereum", "bitcoin"], id="crypto-dropdown"),
-        html.Label("Choose a date range"),
+        html.Label("Choose a date range",
+                   style={
+                       'margin-left': '20px',
+                       'color': 'black',
+                       'font-weight': 'bold'}),
         html.Div([
-            html.Label("Date range"),
             dcc.DatePickerRange(id='date_picker_range'),
         ]),
-        html.Button(id='update-button', children="Update")
-    ], id='left_container'),
+        html.Button(id='update-button', children="Update",
+                    style={'width': '240px', 'height': '40px',
+                           'cursor': 'pointer', 'border': '0px',
+                           'border-radius': '5px', 'background-color':
+                           'black', 'color': 'white', 'text-transform':
+                           'uppercase', 'font-size': '15px'})
+    ], id='left_container',
+        style={'height': '937px',
+               'width': '320px',
+               'background-color': 'white',
+               'float': 'left',
+               'margin': '0'},
+    ),
     html.Div([
-        dcc.Graph(id='crypto_graph')
+        dcc.Graph(id='crypto_graph',
+                  style={"margin-left": "340px"})
     ], id='right-container')
 ])
 
