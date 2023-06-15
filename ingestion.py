6e1461bd6b8c60a750a2fc4e19b93743ef7a3c9c
abdirrahman <abdirrahmang@gmail.com>
2023-06-15 15:53:21 +0100

    set separate times

---
+++ ingestion.py
@@ -0,0 +1,62 @@
+import os
+import requests as req
+from datetime import datetime, timezone, timedelta
+import time
+import json
+from rich.console import Console
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+import numpy as np
+import pandas as pd
+
+CRYPTO = "ethereum"
+INTERVAL = "d1"
+START = "1448064000000"
+END = datetime.now().timestamp() * 1000
+PREVIOUS_MONTH = (datetime.now() - timedelta(days=31)) * 1000
+
+console = Console()
+
+
+def unix_converter():
+    date = datetime(2023, 5, 1)
+
+    return time.mktime(date.timetuple())
+
+def millis_to_datetime(millis: int):
+    date = datetime.fromtimestamp(millis/1000.0, tz=timezone.utc)
+    return date
+
+
+def get_crypto_data(auth_token: str, crypto: str = CRYPTO, interval: str = INTERVAL, start: str = START, end: str = PREVIOUS_MONTH) -> dict[list, str]:
+
+    url = f"https://api.coincap.io/v2/assets/{crypto}/history?interval={interval}&start={start}&end={end}"
+
+    headers = {"X-CoinAPI-Key": auth_token}
+
+    r = req.request("GET", url, headers=headers)
+
+    return r.json()
+
+def get_prediction_data(auth_token: str, crypto: str = CRYPTO, interval: str = INTERVAL, start: str = PREVIOUS_MONTH, end: str = END) -> dict[list, str]:
+
+    url = f"https://api.coincap.io/v2/assets/{crypto}/history?interval={interval}&start={start}&end={end}"
+
+    headers = {"X-CoinAPI-Key": auth_token}
+
+    r = req.request("GET", url, headers=headers)
+
+    return r.json()
+
+
+if __name__ == "__main__":
+    # prerequisites
+    load_dotenv()
+
+    # getting data
+    response = get_crypto_data(auth_token=os.environ['api_key'])
+    date = millis_to_datetime(response['timestamp'])
+    data = response["data"]
+
+    console.print(unix_converter())
+
